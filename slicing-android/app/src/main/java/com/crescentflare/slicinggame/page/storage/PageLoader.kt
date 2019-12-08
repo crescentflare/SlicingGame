a09069f2ade54ac69607e302713dce289ba984ce
Johan Bos <johan@Johans-MacBook.local>
2019-12-08 21:02:30 +0100

    Added online loading support to the page loader

--- slicing-android/app/src/main/java/com/crescentflare/slicinggame/page/storage/PageLoader.kt
+++ slicing-android/app/src/main/java/com/crescentflare/slicinggame/page/storage/PageLoader.kt
@@ -4,12 +4,13 @@ import android.content.Context
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import okhttp3.*
 import java.io.IOException
 
 /**
- * Page storage: handles loading of pages
+ * Page storage: handles loading of pages from several sources
  */
-class PageLoader(private val context: Context, private val location: String) {
+class PageLoader(private val context: Context, location: String, serverPrefix: String = "") {
 
     // --
     // Statics
@@ -26,6 +27,9 @@ class PageLoader(private val context: Context, private val location: String) {
     // Members
     // --
 
+    private val location: String = serverPrefix + location
+    private val entry: String = location
+    private val loadInternal: Boolean = !this.location.contains("://")
     private var loading = false
 
 
@@ -34,16 +38,57 @@ class PageLoader(private val context: Context, private val location: String) {
     // --
 
     fun load(completion: (page: Page?, exception: Throwable?) -> Unit) {
-        val cachedPage = PageCache.getEntry(location)
+        val cachedPage = PageCache.getEntry(entry)
         if (cachedPage != null) {
             completion(cachedPage, null)
-        } else {
+        } else if (loadInternal) {
             loadInternal {
                 if (it != null) {
-                    PageCache.storeEntry(location, it)
+                    PageCache.storeEntry(entry, it)
                 }
                 completion(it, null)
             }
+        } else {
+            loadOnline { page, exception ->
+                if (page != null) {
+                    PageCache.storeEntry(entry, page)
+                }
+                completion(page, exception)
+            }
+        }
+    }
+
+    private fun loadOnline(completion: (page: Page?, exception: Throwable?) -> Unit) {
+        if (!loadInternal) {
+            val client = OkHttpClient()
+            loading = true
+            client.newCall(Request.Builder().url(location).build()).enqueue(object : Callback {
+                override fun onFailure(call: Call, exception: IOException) {
+                    GlobalScope.launch(Dispatchers.Main) {
+                        loading = false
+                        completion(null, exception)
+                    }
+                }
+
+                @Throws(IOException::class)
+                override fun onResponse(call: Call, response: Response) {
+                    val jsonString = response.body()?.string()
+                    if (jsonString != null) {
+                        val page = Page(jsonString)
+                        GlobalScope.launch(Dispatchers.Main) {
+                            loading = false
+                            completion(page, null)
+                        }
+                    } else {
+                        GlobalScope.launch(Dispatchers.Main) {
+                            loading = false
+                            completion(null, null)
+                        }
+                    }
+                }
+            })
+        } else {
+            completion(null, null)
         }
     }
 
@@ -63,11 +108,13 @@ class PageLoader(private val context: Context, private val location: String) {
     }
 
     fun loadInternalSync(): Page? {
-        try {
-            val stream = context.assets.open("pages/$location")
-            val jsonString = stream.bufferedReader().use { it.readText() }
-            return Page(jsonString)
-        } catch (ignored: IOException) {
+        if (loadInternal) {
+            try {
+                val stream = context.assets.open("pages/$location")
+                val jsonString = stream.bufferedReader().use { it.readText() }
+                return Page(jsonString)
+            } catch (ignored: IOException) {
+            }
         }
         return null
     }
