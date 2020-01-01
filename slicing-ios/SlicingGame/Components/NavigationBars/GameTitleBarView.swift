29e3b6cc77556a4046de7cdaaaea29c2ff7867d7
Johan Bos <johan@Johans-MacBook.local>
2020-01-01 17:50:42 +0100

    Added optional image buttons to the game title bar

--- slicing-ios/SlicingGame/Components/NavigationBars/GameTitleBarView.swift
+++ slicing-ios/SlicingGame/Components/NavigationBars/GameTitleBarView.swift
@@ -21,8 +21,10 @@ class GameTitleBarView: LinearContainerView, NavigationBarComponent {
     // --
     
     private var statusUnderlayView: UniView?
-    private var contentView: FrameContainerView?
+    private var contentView: LinearContainerView?
     private var titleView: TextView?
+    private var menuButtonView: ImageButtonView?
+    private var actionButtonView: ImageButtonView?
     private var dividerView: UniView?
 
 
@@ -45,11 +47,22 @@ class GameTitleBarView: LinearContainerView, NavigationBarComponent {
                 // Apply title
                 titleBar.title = convUtil.asString(value: attributes["localizedTitle"])?.localized() ?? convUtil.asString(value: attributes["title"])
                 
+                // Apply icons and events
+                titleBar.menuIcon = ImageSource.fromValue(value: attributes["menuIcon"])
+                titleBar.menuEvent = AppEvent.fromValue(value: attributes["menuEvent"])
+                titleBar.actionIcon = ImageSource.fromValue(value: attributes["actionIcon"])
+                titleBar.actionEvent = AppEvent.fromValue(value: attributes["actionEvent"])
+
                 // Apply bar styling
                 titleBar.showDivider = convUtil.asBool(value: attributes["showDivider"]) ?? false
 
                 // Generic view properties
                 ViewletUtil.applyGenericViewAttributes(convUtil: convUtil, view: titleBar, attributes: attributes)
+
+                // Chain event observer
+                if let eventObserver = parent as? AppEventObserver {
+                    titleBar.eventObserver = eventObserver
+                }
                 return true
             }
             return false
@@ -83,8 +96,10 @@ class GameTitleBarView: LinearContainerView, NavigationBarComponent {
 
         // Bind views
         statusUnderlayView = binder.findByReference("statusUnderlay") as? UniView
-        contentView = binder.findByReference("content") as? FrameContainerView
+        contentView = binder.findByReference("content") as? LinearContainerView
         titleView = binder.findByReference("title") as? TextView
+        menuButtonView = binder.findByReference("menuButton") as? ImageButtonView
+        actionButtonView = binder.findByReference("actionButton") as? ImageButtonView
         dividerView = binder.findByReference("divider") as? UniView
     }
 
@@ -95,9 +110,19 @@ class GameTitleBarView: LinearContainerView, NavigationBarComponent {
     
     override var backgroundColor: UIColor? {
         set {
+            // Apply color to bar
             statusUnderlayView?.backgroundColor = newValue
             contentView?.backgroundColor = newValue
-            titleView?.textColor = averageColor?.intensity() ?? 0 < 0.25 ? .white : .text
+            
+            // Colorize title components
+            let titleColor = averageColor?.intensity() ?? 0 < 0.25 ? UIColor.white : UIColor.text
+            titleView?.textColor = titleColor
+            menuButtonView?.colorize = titleColor
+            actionButtonView?.colorize = titleColor
+            menuButtonView?.highlightedColorize = titleColor.withAlphaComponent(0.5)
+            actionButtonView?.highlightedColorize = titleColor.withAlphaComponent(0.5)
+            menuButtonView?.disabledColorize = titleColor.withAlphaComponent(0.25)
+            actionButtonView?.disabledColorize = titleColor.withAlphaComponent(0.25)
         }
         get { return statusUnderlayView?.backgroundColor }
     }
@@ -109,6 +134,42 @@ class GameTitleBarView: LinearContainerView, NavigationBarComponent {
         get { return titleView?.text }
     }
     
+    var menuIcon: ImageSource? {
+        set {
+            let titlePadding: CGFloat = newValue != nil || actionIcon != nil ? 0 : 16
+            menuButtonView?.source = newValue
+            menuButtonView?.visibility = newValue != nil || actionIcon != nil ? .visible : .hidden
+            actionButtonView?.visibility = newValue != nil || actionIcon != nil ? .visible : .hidden
+            titleView?.padding = UIEdgeInsets(top: 0, left: titlePadding, bottom: 0, right: titlePadding)
+        }
+        get { return menuButtonView?.source }
+    }
+    
+    var menuEvent: AppEvent? {
+        set {
+            menuButtonView?.tapEvent = newValue
+        }
+        get { return menuButtonView?.tapEvent }
+    }
+
+    var actionIcon: ImageSource? {
+        set {
+            let titlePadding: CGFloat = newValue != nil || actionIcon != nil ? 0 : 16
+            actionButtonView?.source = newValue
+            menuButtonView?.visibility = newValue != nil || menuIcon != nil ? .visible : .hidden
+            actionButtonView?.visibility = newValue != nil || menuIcon != nil ? .visible : .hidden
+            titleView?.padding = UIEdgeInsets(top: 0, left: titlePadding, bottom: 0, right: titlePadding)
+        }
+        get { return actionButtonView?.source }
+    }
+    
+    var actionEvent: AppEvent? {
+        set {
+            actionButtonView?.tapEvent = newValue
+        }
+        get { return actionButtonView?.tapEvent }
+    }
+
     var showDivider: Bool = false {
         didSet {
             dividerView?.visibility = showDivider ? .visible : .hidden
@@ -135,6 +196,8 @@ class GameTitleBarView: LinearContainerView, NavigationBarComponent {
     var barContentHeight: CGFloat = 0 {
         didSet {
             contentView?.layoutProperties.height = barContentHeight
+            menuButtonView?.layoutProperties.width = barContentHeight
+            actionButtonView?.layoutProperties.width = barContentHeight
         }
     }
 
